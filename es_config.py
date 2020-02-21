a2779a352d879bec2305a0edcb0fca039aea80b3
Do Nascimento Raul <rdo-nasc@student.42.fr>
2020-02-21 15:25:06 +0100

    travail en cours sur le mapping

--- es_config.py
+++ es_config.py
@@ -1,10 +1,28 @@
 from elasticsearch import Elasticsearch, helpers
-import request_rss
+import request_rss as rrss
+import json
 
 def createIndex(index_name, mapping_file):
     with open(mapping_file) as source:
         mapping = json.load(source)
     es = Elasticsearch()
-    es.indices.create(index_name , body = mapping)
+    return es.indices.create(index_name , body = mapping)
 
-createIndex('feed_rss', 'mappings.json')
+def putBulk(feed_dict):
+    es = Elasticsearch()
+    return helpers.bulk(es, feed_dict)
+
+def gendata(feed_dict):
+    for key, value in feed_dict.items():
+        for doc in value:
+            doc["_index"] = "feed_rss"
+            doc["_type"] = "_doc"
+            yield doc
+
+
+if __name__ == "__main__":
+    #es = Elasticsearch()
+    #es.indices.delete('feed_rss')
+    createIndex('feed_rss', 'mappings.json')
+    feed_dict = rrss.requestFromFile('/home/wamont1-2/Bureau/Python/chap12_elasticsearch/Exercices/es_rss/Flux_RSS_selection.json')
+    putBulk(gendata(feed_dict))
--- request_rss.py
+++ request_rss.py
@@ -1,5 +1,6 @@
 import feedparser
 import json
+import datetime
 
 def requestFromFile(file):
     with open(file) as source:
@@ -13,14 +14,23 @@ def requestFromFile(file):
 def getCleanItem(d, key):
     res = {}
     res['publication'] = key
-    res['date'] = d['published'] if 'published' in d else None
-    res['title'] = d['title'] if 'title' in d else None
-    res['author'] = d['author'] if 'author' in d else None
-    res['description'] = d['summary'] if 'summary' in d else None
-    res['language'] = d['title_detail']['language'] if 'language' in d['title_detail'] else None
-    res['link'] = d['link'] if 'link' in d else None
+    if 'published' in d and d['published']:
+        if key == "technologyreview":
+            res['date'] = datetime.datetime.strptime(d['published'], "%a, %d %b %Y %H:%M:%S %Z").strftime("%Y-%m-%dT%H:%M:%S+0000")
+        else:
+            res['date'] = d['published']
+    if 'title' in d and d['title']:
+        res['title'] = d['title']
+    if 'author' in d and d['author']:
+        res['author'] = d['author'] 
+    if 'summary' in d and d['summary']:
+        res['description'] = d['summary']
+    if 'language' in d['title_detail'] and d['title_detail']['language']:
+        res['language'] = d['title_detail']['language']
+    if 'link' in d and d['link']:
+        res['link'] = d['link']
     return res
 
 if __name__ == "__main__":
     feed_dict = requestFromFile('/home/wamont1-2/Bureau/Python/chap12_elasticsearch/Exercices/es_rss/Flux_RSS_selection.json')
-    print(feed_dict['technologyreview'])
+    #print(feed_dict['technologyreview'])
